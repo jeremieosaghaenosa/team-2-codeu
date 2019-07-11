cd4924097574e193eb614a448021f314052aad60
luisapineda <luisafernanda1997@hotmail.com>
2019-07-11 03:56:56 -0300

    The map is centered in the maps page

--- src/main/java/com/google/codeu/data/MapLocation.java
+++ src/main/java/com/google/codeu/data/MapLocation.java
@@ -4,9 +4,11 @@ package com.google.codeu.data;
  public class MapLocation{
     double lat;
     double lng;
+    double population;
 
-    public MapLocation(double lat, double lng) {
+    public MapLocation(double lat, double lng, double population) {
       this.lat = lat;
       this.lng = lng;
+      this.population = population;
     }
   }
